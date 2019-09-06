d066484b87da2a91e38b35b27a8ef19b51c79aa2
Maciej Drobot <maciejdrobot@gmail.com>
2019-09-06 21:52:09 +0100

    printing tab

---
+++ src/main/java/com/GUI/Printing/BasicInfoController.java
@@ -0,0 +1,4 @@
+package com.GUI.Printing;
+
+public class BasicInfoController {
+}
---
+++ src/main/java/com/GUI/Printing/PrintingController.java
@@ -0,0 +1,33 @@
+package com.GUI.Printing;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PrintingController implements Initializable {
+
+//    @FXML
+//    private Tab printing;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        FXMLLoader loader = new FXMLLoader();
+//        try {
+//            AnchorPane stats = loader.load(getClass().getResource("Stats.fxml"));
+//            printing.setContent(stats);
+//        }
+//        catch(IOException iex)
+//        {
+//            System.out.println("Unable to load");
+//        }
+//
+//        loader = new FXMLLoader();
+
+
+    }
+
+
+}
